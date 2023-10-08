3f22b0588ec2611f09ef93f9d7f214f7fb950e8b
Pavlo Ilnitskyi <inavlo092@gmail.com>
2023-10-08 15:19:05 +0300

    fix student table

--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -1,17 +1,35 @@
 package com.example.demo;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
-@Entity(name ="Student")
+@Entity(name = "Student")
+@Table(
+        name = "student",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "student_email_unique",columnNames = "email")
+        }
+)
 public class Student {
     @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+    @Column(name = "id", updatable = false)
     private Long id;
-    private  String firstName;
-    private  String lastName;
-    private  String email;
-    private  Integer age;
+    @Column(name = "first_name", nullable = false, columnDefinition = "TEXT")
+    private String firstName;
+    @Column(name = "last_name", nullable = false, columnDefinition = "TEXT")
+    private String lastName;
+    @Column(name = "email", nullable = false ,columnDefinition = "TEXT")
+    private String email;
+    @Column(name = "age", nullable = false)
+    private Integer age;
 
     public Student(Long id, String firstName, String lastName, String email, Integer age) {
         this.id = id;
